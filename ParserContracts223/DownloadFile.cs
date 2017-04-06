3842750fd472e24dd13473a77d769e97862f81fe
rummolprod999 <rummolprod999@gmail.com>
2017-04-06 12:21:02 +0300

    update

--- ParserContracts223/DownloadFile.cs
+++ ParserContracts223/DownloadFile.cs
@@ -7,7 +7,9 @@ namespace ParserContracts223
 {
     public class DownloadFile
     {
-        public static string DownL(string url)
+        static bool DownCount = true;
+
+        public static string DownLOld(string url)
         {
             int ind = url.LastIndexOf("/");
             string namearch = url.Substring(ind + 1);
@@ -35,6 +37,77 @@ namespace ParserContracts223
             }
 
             return "";
+        }
+
+        static void DownloadF(string sSourceURL, string sDestinationPath)
+        {
+            long iFileSize = 0;
+            int iBufferSize = 1024;
+            iBufferSize *= 1000;
+            long iExistLen = 0;
+            FileStream saveFileStream;
+            if (File.Exists(sDestinationPath))
+            {
+                FileInfo fINfo =
+                    new FileInfo(sDestinationPath);
+                iExistLen = fINfo.Length;
+            }
+            if (iExistLen > 0)
+                saveFileStream = new FileStream(sDestinationPath,
+                    FileMode.Append, FileAccess.Write,
+                    FileShare.ReadWrite);
+            else
+                saveFileStream = new FileStream(sDestinationPath,
+                    FileMode.Create, FileAccess.Write,
+                    FileShare.ReadWrite);
+
+            HttpWebRequest hwRq;
+            HttpWebResponse hwRes;
+            hwRq = (HttpWebRequest) HttpWebRequest.Create(sSourceURL);
+            hwRq.AddRange((int) iExistLen);
+            try
+            {
+                Stream smRespStream;
+                hwRes = (HttpWebResponse) hwRq.GetResponse();
+                smRespStream = hwRes.GetResponseStream();
+                iFileSize = hwRes.ContentLength;
+                int iByteSize;
+                byte[] downBuffer = new byte[iBufferSize];
+
+                while ((iByteSize = smRespStream.Read(downBuffer, 0, downBuffer.Length)) > 0)
+                {
+                    saveFileStream.Write(downBuffer, 0, iByteSize);
+                }
+            }
+            catch (WebException ex)
+            {
+                hwRes = (HttpWebResponse) ex.Response;
+                if (hwRes != null && (hwRes.StatusCode == HttpStatusCode.RequestedRangeNotSatisfiable ||
+                                      hwRes.StatusCode == HttpStatusCode.NotFound))
+                {
+                    DownCount = false;
+                }
+            }
+        }
+
+        public static string DownL(string url)
+        {
+            int ind = url.LastIndexOf("/");
+            string namearch = url.Substring(ind + 1);
+            string patharch = $"./{Program.Tempdir}/{namearch}";
+            while (DownCount)
+            {
+                try
+                {
+                    DownloadF(url, patharch);
+                }
+                catch (Exception e)
+                {
+                    Log.Logger(e);
+                }
+            }
+
+            return patharch;
         }
     }
 }
